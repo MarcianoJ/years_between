f378ee08075cf64c19c219e224bee63270894d31
Marciano Jorden <marciano@ivaldi.nl>
2020-10-07 13:56:14 +0200

    Add method to calculate years

---
+++ lib/years_between.rb
@@ -0,0 +1,56 @@
+require 'date'
+
+class YearsBetween
+  def self.calculate_years_between(start_date, end_date, exclude: nil)
+    start_date += 1 if exclude.to_s == 'start_date' || exclude.to_s == 'both'
+    end_date -= 1 if exclude.to_s == 'end_date' || exclude.to_s == 'both'
+
+    raise 'start_date and end_date must be present' if [start_date, end_date].any?(&:nil?)
+    raise 'start_date cannot be past end_date' if start_date > end_date
+
+    start_year = start_date.year
+    start_month = start_date.month
+    start_day = start_date.day
+
+    end_year = end_date.year
+    end_month = end_date.month
+    end_day = end_date.day
+
+    start_mmdd = start_month * 100 + start_day
+    end_mmdd = end_month * 100 + end_day
+
+    if end_mmdd >= start_mmdd
+      anniversary_year = end_year
+    else
+      anniversary_year = end_year - 1
+    end
+
+    if start_month == 2 && start_day == 29 && !(Date.leap?(anniversary_year))   
+      anniversary_month = 3
+      anniversary_day = 1
+    else
+      anniversary_month = start_month
+      anniversary_day = start_day
+    end
+
+    year_frac_new = anniversary_year - start_year
+
+    no_of_days = (end_date - Date.new(anniversary_year, anniversary_month, anniversary_day)).to_i + 1
+
+    if start_mmdd <= 229
+      if Date.leap?(anniversary_year)
+        tot_no_of_days = 366
+      else
+        tot_no_of_days = 365
+      end
+    else        
+      if Date.leap?(anniversary_year + 1)
+        tot_no_of_days = 366
+      else
+        tot_no_of_days = 365
+      end
+    end
+
+    year_frac_new = year_frac_new + (no_of_days.to_f / tot_no_of_days.to_f)
+  end
+end
---
+++ years_between.gemspec
@@ -0,0 +1,11 @@
+Gem::Specification.new do |s|
+  s.name        = 'years_between'
+  s.version     = '1.0.0'
+  s.date        = '2020-10-07'
+  s.summary     = "Calculates years between dates including decimals"
+  s.description = "Calculates years between dates including decimals"
+  s.authors     = ["Ivaldi"]
+  s.files       = ["lib/years_between.rb"]
+  s.homepage    =
+    'https://rubygems.org/gems/years_between'
+end
